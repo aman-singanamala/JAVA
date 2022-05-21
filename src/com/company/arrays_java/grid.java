f74a2765f2442472c13f9af275b9a985e695f076
Aman <amansinganamala@gmail.com>
2022-05-21 21:47:01 +0530

    average

---
+++ src/com/company/arrays_java/grid.java
@@ -0,0 +1,17 @@
+package com.company.arrays_java;
+
+import java.util.Scanner;
+
+public class grid {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n;
+        n= in.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("- ");
+            }
+            System.out.println();
+        }
+    }
+}
