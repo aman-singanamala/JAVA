4a0018c55a3797df2420e0bf2287d361d0240ba3
Aman <amansinganamala@gmail.com>
2022-05-30 11:00:20 +0530

    commit

---
+++ src/com/company/leetcode/two_sum.java
@@ -0,0 +1,22 @@
+package com.company.leetcode;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class two_sum {
+    public static void main(String[] args) {
+        int[] a={2,3,4};
+        int target=6;
+        int[] ans=func(a,target);
+        System.out.println(Arrays.toString(ans));
+    }
+    public static  int[] func(int[] nums,int target){
+        for(int i=0;i< nums.length;i++){
+            for(int j=i+1;j< nums.length;j++){
+                if((i!=j)&& nums[i]+nums[j]==target){
+                    return new int[] {i,j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
