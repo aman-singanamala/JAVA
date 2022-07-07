306bd430d3b55f4ccec1666f053a41698b6d47b4
aman-singanamala <singanamala.aman2020@vitstudent.ac.in>
2022-07-07 21:22:40 +0530

    commit

---
+++ src/com/company/leetcode/problem_no_7.java
@@ -0,0 +1,21 @@
+package com.company.leetcode;
+
+public class problem_no_7 {
+    public int reverse(int x){
+
+        long rev=0;
+        if(x==0){
+            return 0;
+        }
+        while (x!=0){
+            int r= x%10;
+            rev=rev*10+r;
+            x=x/10;
+        }
+        if(rev>Integer.MAX_VALUE|| rev<Integer.MIN_VALUE){
+            return 0;
+        }
+        return (int) rev;
+    }
+
+}
