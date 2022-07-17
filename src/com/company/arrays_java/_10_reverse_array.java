9613d6e41bdd6ca4d9ab359a45cc1d86c0376b0c
aman-singanamala <84006448+aman-singanamala@users.noreply.github.com>
2022-07-17 20:42:32 +0530

    Create _10_reverse_array.java

--- src/com/company/arrays_java/_10_reverse_array.java
+++ src/com/company/arrays_java/_10_reverse_array.java
@@ -1,14 +1,18 @@
 package com.company.arrays_java;
-
 public class _10_reverse_array {
     public static void reverse(int[] Array){
         for(int i=Array.length-1;i>=0;i--){
             System.out.print(Array[i]+" ");
         }
     }
-
     public static void main(String[] args) {
         int[] arr ={1,2,3,6,5,9,8,7,4,3,1,2,22,111};
         reverse(arr);
     }
 }
+
+
+
+
+
+//Reverse
