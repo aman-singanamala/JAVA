709243b14b2982a78562d1a4e593165e9c98b0a1
aman-singanamala <singanamala.aman2020@vitstudent.ac.in>
2022-07-30 20:01:45 +0530

    commit

--- src/com/company/LinkedList/PRACTICE.java
+++ src/com/company/LinkedList/PRACTICE.java
@@ -54,7 +54,6 @@ public class PRACTICE {
 //        }
 //        temp.next= newNode;
 //    }
-
     public void insertIndex(int val,int index){
         if(index==0){
             insert(val);
@@ -76,6 +75,60 @@ public class PRACTICE {
         temp.next= newNode;
         size++;
     }
+    public int deleteFirst(){
+        int val= head.data;
+        head= head.next;
+        if(head==null){
+            tail=null;
+        }
+        size--;
+        return val;
+    }
+    //Find the node with a given value O(n)
+    public Node find(int value){
+        Node node= head;
+        while(node!=null){
+            if(node.data==value){
+                return node;
+            }
+            node=node.next;
+        }
+        return null;
+    }
+    // get the node at a given index
+    public Node get(int index){
+        Node node = head;
+        for(int i=0;i<index;i++){
+            node=node.next;
+        }
+        return node;
+    }
+    public int deleteLast(){
+        if(size<=1){
+            return deleteFirst();
+        }
+        Node secondLast= get(size-2);
+        int val= tail.data;
+
+        tail=secondLast;
+        tail.next=null;
+        size--;
+        return val;
+    }
+
+    public int deleteIndex(int index){
+        if(index==0){
+            return deleteFirst();
+        }
+        if(index==size-1){
+            return deleteLast();
+        }
+        Node prev = get(index-1);
+        int val=prev.next.data;
+        prev.next= prev.next.next;
+        size--;
+        return val;
+    }
 
     public void display(){
         Node temp = head;
@@ -88,13 +141,31 @@ public class PRACTICE {
     }
     public static void main(String[] args) {
         PRACTICE list = new PRACTICE();
-        list.insert(11);
-        list.insert(12);
-        list.insert(13);
-        list.insert(14);
-        list.insertLast(15);
-        list.insertIndex(10,0);
-        list.insertIndex(16,6);
+//        list.insert(11);
+//        list.insert(12);
+//        list.insert(13);
+//        list.insert(14);
+//        list.insertLast(15);
+//        list.insertIndex(10,0);
+//        list.insertIndex(16,6);
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.insert(4);
+        list.insert(5);
+        list.insert(6);
+        list.insert(7);
+        list.insert(8);
         list.display();
+        //4 -> 3 -> 2 -> 1 -> END
+        System.out.println("Item deleted at first is : "+list.deleteFirst());
+        list.display();
+        System.out.println("Item deleted at last is : "+list.deleteLast());
+        list.display();
+        System.out.println("Item deleted at index 5 is :"+list.deleteIndex(5));
+        list.display();
+        System.out.println(list.size);
+        Node a= list.find(4);
+        System.out.println(a.data);
     }
 }
