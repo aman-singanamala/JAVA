cd0c5dfe5f26ac2db524d6580eca12ecf51f8e8d
aman-singanamala <singanamala.aman2020@vitstudent.ac.in>
2022-07-31 22:37:23 +0530

    commit

---
+++ src/com/company/leetcode/problem_no_206.java
@@ -0,0 +1,18 @@
+//package com.company.leetcode;
+//
+//public class problem_no_206 {
+//    public class ListNode{
+//        int val;
+//        ListNode next;
+//        ListNode(int val){
+//            this.val=val;
+//        }
+//        public ListNode(int val, ListNode next) {
+//            this.val = val;
+//            this.next = next;
+//        }
+//    }
+//    public ListNode reverseList(ListNode head){
+//
+//    }
+//}
