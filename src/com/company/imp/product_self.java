e0ee17e5b76c7b6a29e90e3bf60907557ea9c66f
aman-singanamala <singanamala.aman2020@vitstudent.ac.in>
2022-09-19 22:04:32 +0530

    subarray

---
+++ src/com/company/imp/product_self.java
@@ -0,0 +1,25 @@
+package com.company.imp;
+import java.util.Arrays;
+public class product_self {
+    static int[] func(int[] array,int n){
+        int[] ans= new int[n];
+        int k=0;
+        for(int i=0;i<n;i++){
+            int product= 1;
+            for(int j=0;j<n;j++){
+                if(i==j){
+                    continue;
+                }
+                product= product*array[j];
+            }
+            ans[i]= product;
+        }
+        return ans;
+    }
+    public static void main(String[] args){
+        int[] arr= {1,2,3,4,5};
+        int n= arr.length;
+        int[] ans= func(arr,n);
+        System.out.println(Arrays.toString(ans));
+    }
+}
