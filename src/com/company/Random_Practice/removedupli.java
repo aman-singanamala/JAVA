0137aff6bc3028ffec445422dbb77ebe5fea212b
aman-singanamala <singanamala.aman2020@vitstudent.ac.in>
2023-02-01 19:19:10 +0530

    feb-1 solved questions

--- src/com/company/Random_Practice/removedupli.java
+++ src/com/company/Random_Practice/removedupli.java
@@ -15,6 +15,5 @@ public class removedupli {
          for(Character c: ans){
              System.out.println(c);
          }
-
     }
 }
--- src/com/company/arrays_java/_1_sum_of_values_in_array.java
+++ src/com/company/arrays_java/_1_sum_of_values_in_array.java
@@ -19,7 +19,5 @@ public class _1_sum_of_values_in_array {
         System.out.println("Sum of elements in the arrays is:");
         add(array);
         // addition
-
-
     }
 }
